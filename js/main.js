6e2f47dfa953319ab7dc7584873b5282d08723a8
dudqls611 <dudqls611@naver.com>
2023-05-08 21:34:26 +0900

    코딩중!!

--- js/main.js
+++ js/main.js
@@ -29,3 +29,36 @@ function hideBasket() {
 
 
 // 검색
+const headerEl = document.querySelector('header')
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')] // ...전개연산자를 배열로 뿌리는 과정
+const searchWrapEl = headerEl.querySelector('.search-wrap')
+const searchStarterEl = headerEl.querySelector('.search-starter')
+const searchCloserEl = searchWrapEl.querySelector('.search-closer')
+const searchShadowEl = searchWrapEl.querySelector('.shadow')
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
+
+searchStarterEl.addEventListener('click', showSearch)
+searchCloserEl.addEventListener('click', hideSearch)
+searchShadowEl.addEventListener('click', hideSearch) //콜백함수 사용
+
+function showSearch() {
+    headerEl.classList.add('searching')
+    document.documentElement.classList.add('fixed')
+    headerMenuEls.reverse().forEach(function(el, index) {
+        el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
+    })
+    searchDelayEls.forEach(function(el, index) {
+        el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+    })
+}
+function hideSearch() {
+    headerEl.classList.remove('searching')
+    document.documentElement.classList.remove('fixed')
+    headerMenuEls.reverse().forEach(function(el, index) {
+        el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
+    })
+    searchDelayEls.reverse().forEach(function(el, index) {
+        el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+    })
+    searchDelayEls.reverse()
+}
